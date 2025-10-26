f248b78fb0bdf97048626734d5c202eae3d35f4c
Reno Angelo Banderlipe <personal.renoangelo@gmail.com>
2025-10-26 13:50:24 +0800

    feat: Settings Page

---
+++ app/admin/settings/components/ProfileInfo.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { FormProfileUploader } from "@/components/shared/form-profile-uploader";
+import { FormSwitchField } from "@/components/shared/form-switch-field";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  type UpdateProfileDTO,
+  UpdateProfileSchema,
+} from "@/features/settings/dtos/updateProfile";
+
+export const ProfileInfo = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { dirtyFields },
+  } = useForm<UpdateProfileDTO>({
+    resolver: zodResolver(UpdateProfileSchema),
+    defaultValues: {
+      photo: undefined,
+      emailVerified: false,
+    },
+  });
+
+  const onSubmit = (data: UpdateProfileDTO) => {
+    console.log("Form submitted:", data);
+  };
+  return (
+    <Card className="flex-1">
+      <CardHeader></CardHeader>
+      <CardContent className="space-y-6">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+          <FormProfileUploader
+            control={control}
+            name="photo"
+            label="Profile Photo"
+          />
+          <FormSwitchField
+            control={control}
+            name="emailVerified"
+            label="Email verified"
+            description="Disabling this will automatically send the user a verification email."
+          />
+          {(!!dirtyFields.photo || !!dirtyFields.emailVerified) && (
+            <Button type="submit" className="w-full">
+              Save changes
+            </Button>
+          )}
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
