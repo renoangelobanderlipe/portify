19a00d244fbce12eaa7b3970796ba1d5061fa65a
Reno Angelo Banderlipe <personal.renoangelo@gmail.com>
2025-10-27 22:49:25 +0800

    chore: fix type error

--- app/admin/api-keys/components/Pagination.tsx
+++ app/admin/api-keys/components/Pagination.tsx
@@ -14,7 +14,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import type { Payment } from "./ApiKeyDataTable";
+import type { Payment } from "./columns";
 
 type ApiKeyPaginationProps = {
   table: Table<Payment>;
