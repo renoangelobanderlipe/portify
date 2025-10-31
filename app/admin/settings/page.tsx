4213fd3a748d5bd8b64e1761a4a12c4cdfb0fc0f
Reno Angelo Banderlipe <92479031+renoangelobanderlipe@users.noreply.github.com>
2025-10-31 15:03:48 +0800

    fix: tech stack layout (#22)
    Co-authored-by: Reno Angelo Banderlipe <personal.renoangelo@gmail.com>

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -5,25 +5,25 @@ import { SettingsCard } from "./components/Settings/SettingsCard";
 import TechStackCard from "./components/TechStack/TechStackCard";
 import { ProfileCard } from "./components/UserProfile/ProfileCard";
 
-export default function Page() {
-  const tabs = [
-    {
-      value: "account",
-      label: "Account",
-      content: <ProfileCard />,
-    },
-    {
-      value: "tech-stack",
-      label: "Tech Stack",
-      content: <TechStackCard />,
-    },
-    {
-      value: "settings",
-      label: "Settings",
-      content: <SettingsCard />,
-    },
-  ];
+const tabs = [
+  {
+    value: "account",
+    label: "Account",
+    content: <ProfileCard />,
+  },
+  {
+    value: "tech-stack",
+    label: "Tech Stack",
+    content: <TechStackCard />,
+  },
+  {
+    value: "settings",
+    label: "Settings",
+    content: <SettingsCard />,
+  },
+];
 
+export default function Page() {
   return (
     <Tabs defaultValue="account">
       <TabsList className="bg-transparent">
